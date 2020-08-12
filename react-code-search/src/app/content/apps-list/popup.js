e063e1631d9a2fc298e245fafdfede3c31c1473e
unknown <wuchen@DESKTOP-H44RLV9.fareast.corp.microsoft.com>
2020-08-12 08:52:29 +0800

    new file:   react-code-search/src/app/content/apps-list/popup.js 	new file:   react-code-search/src/app/content/apps-list/style.css 	new file:   react-code-search/src/assets/icons/search.svg

---
+++ react-code-search/src/app/content/apps-list/popup.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './style.css';
+
+const Popup = props => {
+  return (
+    <div className="popup-box">
+      <div className="box">
+        <span className="close-icon" onClick={props.handleClose}>x</span>
+        {props.content}
+      </div>
+    </div>
+  );
+};
+
+export default Popup;
