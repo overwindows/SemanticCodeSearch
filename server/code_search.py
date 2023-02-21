bcf9ffd2fbacc61be476bb0f1a73b0c95934f169
Chen Wu <overwindows@gmail.com>
2023-02-21 16:53:10 +0800

    add model path and data path

--- server/code_search.py
+++ server/code_search.py
@@ -23,9 +23,9 @@ Translation = namedtuple('Translation', 'src_str hypos pos_scores alignments')
 
 
 class CodeSearch(object):
-    def __init__(self):
+    def __init__(self, model_path):
         self.args = None
-        self.local_model_path = '/home/overwindows/neuralbow_hybrid-2020-07-05-13-43-57_model_best.pkl.gz'
+        self.local_model_path = model_path
         self.indices = {}
         self.definitions = {}
         self.model = None
@@ -47,7 +47,7 @@ class CodeSearch(object):
                  })
         return predictions
 
-    def load_model(self):
+    def load_model(self, data_path):
         model_path = RichPath.create(self.local_model_path, None)
         print("Restoring model from %s" % model_path)
         self.model = model_restore_helper.restore(
@@ -55,19 +55,19 @@ class CodeSearch(object):
             is_train=False,
             hyper_overrides={})
 
-        # for language in ['python', 'go', 'javascript', 'java', 'php', 'ruby']:
-        for language in ['go']:
+        for language in ['python', 'go', 'javascript', 'java', 'php', 'ruby']:
+        # for language in ['ruby']:
             print("Loading language: %s" % language)
             """
             wget https://s3.amazonaws.com/code-search-net/CodeSearchNet/v2/{language}.zip
             """
             self.definitions[language] = pickle.load(
-                open('/home/overwindows/{}_dedupe_definitions_v2.pkl'.format(language), 'rb'))
+                open(data_path+'/{}_dedupe_definitions_v2.pkl'.format(language), 'rb'))
 
-            if os.path.exists('/home/overwindows/{}.ann'.format(language)):
+            if os.path.exists(data_path+'/{}.ann'.format(language)):
                 self.indices[language] = AnnoyIndex(128, 'angular')
                 self.indices[language].load(
-                    '/home/overwindows/{}.ann'.format(language))
+                    data_path+'/{}.ann'.format(language))
             else:
                 indexes = [{'code_tokens': d['function_tokens'],
                             'language': d['language']} for d in tqdm(self.definitions[language])]
@@ -81,4 +81,4 @@ class CodeSearch(object):
                     self.indices[language].add_item(index, vector)
                 self.indices[language].build(1000)
                 self.indices[language].save(
-                    '/home/overwindows/{}.ann'.format(language))
+                    data_path+'/{}.ann'.format(language))
--- server/main.py
+++ server/main.py
@@ -1,14 +1,13 @@
 #!/usr/local/bin/python3
 import json
-
+import argparse
 from flask import Flask, abort, jsonify, request
 from flask_cors import CORS, cross_origin
-from server.code_search import CodeSearch 
+from server.code_search import CodeSearch
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-CODESERACH = CodeSearch()
 
 '''
 generator = None
@@ -17,6 +16,7 @@ max_positions = None
 align_dict = None
 '''
 
+
 @app.route("/search", methods=['GET'])
 @cross_origin()
 def search():
@@ -24,6 +24,7 @@ def search():
     print(query)
     json_res = []
     for lang in ['python', 'go', 'javascript', 'java', 'php', 'ruby']:
+    # for lang in ['ruby']:
         sample_json = CODESERACH.search(query, language=lang)
         json_res.extend(sample_json)
         # if lang == 'python':
@@ -35,54 +36,18 @@ def search():
     return jsonify(json_res)
 
 
-@app.route("/translate", methods=['POST'])
-@cross_origin()
-def translate():
-    data = request.get_json()
-    # print(data)
-    trans_code = CODETRANS.translate(data['code'])
-    return jsonify({'cpp': trans_code})
-    # str_code = None
-    # result = None
-    # if 'text' not in data or len(data['text']) == 0 or 'model' not in data:
-    #     abort(400)
-    # else:
-    #     text = data['text']
-    #     model = data['model']
-    #     # print(text)
-    #     if args.spm:
-    #         str_code = ' '.join(sp.EncodeAsPieces(normalizeSnippet(text)))
-    #     else:
-    #         str_code = normalizeSnippet(text, tokenize=True)
-    #     #print(str_code, sp.DecodePieces(str_code.split()))
-    #     ret = patchgen.generate_patch(str_code)
-    #     # print(ret)
-    #     if args.spm:
-    #         result = ''.join(sp.DecodePieces(ret.split()))
-    #     else:
-    #         result = decodeSnippet(ret)
-    #     '''
-    #     result = generate_text(
-    #         model_type='gpt2',
-    #         length=100,
-    #         prompt=text,
-    #         model_name_or_path=model
-    #     )
-    #     '''
-    #     return jsonify({'result': result})
-
+argparse = argparse.ArgumentParser()
+argparse.add_argument('--model_path', type=str,
+                      default='/root/neuralbow_hybrid-2020-07-05-13-43-57_model_best.pkl.gz')
+argparse.add_argument('--data_path', type=str, default='/root/')
+args = argparse.parse_args()
 
 if __name__ == "__main__":
-    #parser = options.get_generation_parser(interactive=True)
-    #args = options.parse_args_and_arch(parser)
-    CODESERACH.load_model()
-    # CODETRANS.load_model()
+    CODESERACH = CodeSearch(args.model_path)
+    CODESERACH.load_model(args.data_path)
     print('loading completed.')
-    # codetrans.translate(python_code)
-    # if args.spm:
-    # sp.Load('sentencepiece.bpe.model')
-    #    sp.load(args.spm)
+
     try:
-        app.run(host='0.0.0.0', debug=False, use_reloader=False)
+        app.run(host='0.0.0.0', debug=False, use_reloader=False, port=8000)
     except KeyboardInterrupt as e:
         print("[STOP]", e)
